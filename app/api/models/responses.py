472dae345fa6e45c70b5dec45d788d9a5925d4ea
thnndat236 <thnndat236@gmail.com>
2025-10-02 19:45:01 +0700

    add pytest-cov pytest-asyncio plugins

--- app/api/models/responses.py
+++ app/api/models/responses.py
@@ -1,4 +1,4 @@
-from fastapi.responses import JSONResponse
+# from fastapi.responses import JSONResponse
 
-def error_response(message: str, status_code: int = 400):
-    return JSONResponse(content={"error": message}, status_code=status_code)
+# def error_response(message: str, status_code: int = 400):
+#     return JSONResponse(content={"error": message}, status_code=status_code)
